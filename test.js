55531be9bf91cde4b6cd4cf5fddca8e06a45317b
Ben VG <ben@benvg.com>
2016-08-26 15:09:14 -0500

    created a test web server in Node

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
-This is a test idea
+//Lets require/import the HTTP module
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+//Lets define a port we want to listen to
+const PORT=8080; 
 
-test idea 
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+  	response.end(index);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+
